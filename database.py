1766117e38ad61f7b3650054126634f01bd6e3e1
prashanthe79-python <prashanthe79@gmail.com>
2025-05-31 14:54:52 +0530

    updated

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:SSLWqQEjFBnbGqBFByNFDzaeYXlyszvX@turntable.proxy.rlwy.net:38794/railway")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://create_contacts_ef84_user:JgJ9yWGhbtwhI5eKT7Rinq6wuKPjSZwq@dpg-d0tc4lidbo4c739h6980-a/create_contacts_ef84")
 
 # Sync SQLAlchemy engine (for table creation, etc.)
 engine = create_engine(DATABASE_URL)
